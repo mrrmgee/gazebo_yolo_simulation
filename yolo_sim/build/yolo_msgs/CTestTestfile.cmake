f90dbceb3360d4a2d15abab6b16fb1ee0e15570e
mrrmgee <jglaw2023@gmail.com>
2025-02-03 14:47:55 -0500

    colcon build the submodule

---
+++ yolo_sim/build/yolo_msgs/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/src/yolo_ros_aav/yolo_msgs
+# Build directory: /home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("yolo_msgs__py")
---
+++ yolo_sim/build/yolo_msgs/yolo_msgs__py/CMakeFiles/yolo_msgs__py.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/_yolo_msgs_s.ep.rosidl_typesupport_c.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/_yolo_msgs_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/_yolo_msgs_s.ep.rosidl_typesupport_introspection_c.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/__init__.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_bounding_box2_d.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_bounding_box2_d_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_bounding_box3_d.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_bounding_box3_d_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_detection.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_detection_array.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_detection_array_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_detection_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_key_point2_d.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_key_point2_d_array.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_key_point2_d_array_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_key_point2_d_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_key_point3_d.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_key_point3_d_array.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_key_point3_d_array_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_key_point3_d_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_mask.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_mask_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_point2_d.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_point2_d_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_pose2_d.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_pose2_d_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_vector2.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/msg/_vector2_s.c"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/srv/__init__.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/srv/_set_classes.py"
+  "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/yolo_msgs/rosidl_generator_py/yolo_msgs/srv/_set_classes_s.c"
+  "CMakeFiles/yolo_msgs__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/yolo_msgs__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
