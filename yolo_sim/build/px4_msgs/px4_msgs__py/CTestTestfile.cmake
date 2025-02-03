3d049e7b709d00914b46667fed20d8d0cd4088bf
mrrmgee <jglaw2023@gmail.com>
2025-02-03 14:18:08 -0500

    built enviro with px4 msg

---
+++ yolo_sim/build/px4_msgs/px4_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/px4_msgs/px4_msgs__py
+# Build directory: /home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/build/px4_msgs/px4_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ yolo_sim/build/px4_ros_com/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/jackson/Final_simulation_full/gazebo_yolo_simulation/yolo_sim/install/px4_msgs/share/px4_msgs/package.sh"
