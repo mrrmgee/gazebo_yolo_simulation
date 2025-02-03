53514cab0dc8700dcfa59fe89aae48d2de13c2a5
mrrmgee <jglaw2023@gmail.com>
2025-02-03 15:01:11 -0500

    changed yolo startup script to use this workspace

--- yolo_startup_simulated.sh
+++ yolo_startup_simulated.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd
-cd ~/ros2_ws/
+cd ~/Final_simulation_full/gazebo_yolo_simulation/yolo_sim
 source /opt/ros/humble/setup.bash
 source install/local_setup.bash
 ros2 launch yolo_bringup yolov8.launch.py
